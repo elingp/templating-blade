750517e64f364ddd344bfa7021f3f975b95702b7
Eling Pramuatmaja <elingpramuatmaja@gmail.com>
2020-07-02 07:33:51 +0700

    Add AdminLTE template and the assigned task

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('items.index');
+});
+
+Route::get('/data-tables', function () {
+    return view('items.data-tables');
 });
